bcbe14dfd0437295aaad887473019bb85f078286
Michalis Kargakis <kargakis@protonmail.com>
2024-10-02 12:20:22 +0200

    Update to valid subscription IDs

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,13 +3,13 @@ import { ethers } from "hardhat";
 const BASE_SEPOLIA_SETTINGS = {
   keyHash: "0x9e1344a1247c8a1785d0a4681a27152bffdb43666ae5bf7d14d24a5efd44bf71",
   vrfCoordinator: "0x5C210eF41CD1a72de73bF76eC39637bB0d3d7BEE",
-  subscriptionId: 3330
+  subscriptionId: 183
 };
 
 const BASE_SETTINGS = {
   keyHash: "0x00b81b5a830cb0a4009fbd8904de511e28631e62ce5ad231373d3cdad373ccab",
   vrfCoordinator: "0xd5D517aBE5cF79B7e95eC98dB0f0277788aFF634",
-  subscriptionId: 3330
+  subscriptionId: 26
 };
 
 export async function deployContract() {
