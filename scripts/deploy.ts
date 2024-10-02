7c4b1527ab207127bb5081be7ddd2df43c1955c7
Michalis Kargakis <kargakis@protonmail.com>
2024-10-02 12:13:36 +0200

    Update deployment script to use Chainlink v2.5 config on Base

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,24 @@
 import { ethers } from "hardhat";
 
-const GOERLI_SETTINGS = {
-  keyHash:"0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-  vrfConsumer:"0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
-  subscriptionId:3330
-}
-const MAINNET_SETTINGS = {
-  keyHash:"0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef",
-  vrfConsumer:"0x271682DEB8C4E0901D1a1550aD2e64D568E69909",
-  subscriptionId:3330
-}
+const BASE_SEPOLIA_SETTINGS = {
+  keyHash: "0x9e1344a1247c8a1785d0a4681a27152bffdb43666ae5bf7d14d24a5efd44bf71",
+  vrfCoordinator: "0x5C210eF41CD1a72de73bF76eC39637bB0d3d7BEE",
+  subscriptionId: 3330
+};
+
+const BASE_SETTINGS = {
+  keyHash: "0x00b81b5a830cb0a4009fbd8904de511e28631e62ce5ad231373d3cdad373ccab",
+  vrfCoordinator: "0xd5D517aBE5cF79B7e95eC98dB0f0277788aFF634",
+  subscriptionId: 3330
+};
 
 export async function deployContract() {
-
   const prod = false;
-  const settings = prod ? MAINNET_SETTINGS : GOERLI_SETTINGS
-  const { keyHash, vrfConsumer, subscriptionId } = settings
+  const settings = prod ? BASE_SETTINGS : BASE_SEPOLIA_SETTINGS
+  const { keyHash, vrfCoordinator, subscriptionId } = settings
 
   const Crylot = await ethers.getContractFactory("Crylot");
-  const lock = await Crylot.deploy(keyHash, vrfConsumer, subscriptionId);
+  const lock = await Crylot.deploy(keyHash, vrfCoordinator, subscriptionId);
 
   console.log("Contract deployed to: " + lock.address)
   return lock.deployed();
